361b0a50cb981a6e30c01341b9f0770ec8648f74
l1728 <cheburashka077@ukr.net>
2024-02-22 22:13:57 +0200

    finish

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,4 @@
-const getUsersWithFriend = (users, friendName) => users.filter(user => user.name === friendName);
+const getUsersWithFriend = (users, friendName) => users.filter(user => user.friends.includes(friendName));
 
 
 const allUsers = [
